8d74469307f62f2c381667ace54442b3549be1e5
Miguel Kerschkamp <99051637@mydavinci.nl>
2019-01-22 10:58:17 +0100

    Initial commit

---
+++ JS/Responsive.js
@@ -0,0 +1,12 @@
+
+
+function menu1() {
+ document.getElementById("dropdown").classList.toggle("Dshow");	
+
+}
+
+function menu2() {
+ document.getElementById("dropdown2").classList.toggle("Dshow");	
+
+
+}
